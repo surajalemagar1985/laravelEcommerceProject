31a50c04e73cdd9ff4a00eb2c3ee3e0ddeb6ae7c
surajalemagar1985 <surajale1985@gmail.com>
2021-03-18 08:42:09 +0545

    thirdcommit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('user.index');
+    }
+
+    public function about()
+    {
+        return view('user.about');
+    }
+
+    public function service()
+    {
+        return view('user.service');
+    }
+
+    public function portfolio()
+    {
+        return view('user.portfolio');
+    }
+
+    public function team()
+    {
+        return view('user.team');
+    }
+
+    public function pricing()
+    {
+        return view('user.pricing');
+    }
+
+    public function blog()
+    {
+        return view('user.blog');
+    }
+    public function contact()
+    {
+        return view('user.contact');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/index','App\Http\Controllers\UserController@index')->name('index');
+Route::get('/about','App\Http\Controllers\UserController@about')->name('about');
+Route::get('/service','App\Http\Controllers\UserController@service')->name('service');
+Route::get('/portfolio','App\Http\Controllers\UserController@portfolio')->name('portfolio');
+Route::get('/team','App\Http\Controllers\UserController@team')->name('team');
+Route::get('/pricing','App\Http\Controllers\UserController@pricing')->name('pricing');
+Route::get('/blog','App\Http\Controllers\UserController@blog')->name('blog');
+Route::get('/contact','App\Http\Controllers\UserController@contact')->name('contact');
+
+
+
